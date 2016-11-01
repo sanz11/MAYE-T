2c8a74b3ca0bafd4b275e0a0dc23731883dfefa5
Abel <abelricrasanchez@gmail.com>
2016-11-01 16:45:46 -0500

    elimine un documento word colado ijji

--- vista/listar_trabajo.php
+++ vista/listar_trabajo.php
@@ -9,6 +9,7 @@
 	<link rel="stylesheet" href="../css/menu.css">
 	<link rel="stylesheet" href="../css/fontello.css">
 	<link rel="stylesheet" href="../css/lista.css">
+	<link rel="stylesheet" href="../css/login.css">
 
 </head>
 <body>
