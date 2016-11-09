52497376c14821054c36bad4b9f3773512cde2c9
Abel <abelricrasanchez@gmail.com>
2016-11-09 03:40:29 -0500

    El registro.php modificado

--- controlador/registro.php
+++ controlador/registro.php
@@ -4,6 +4,8 @@ require_once("../modelo/trabajador.php");
 
 $trabajo= new trabajador();
 
+
+
 @$nombre=$_POST['nombre'];
 @$apellidos=$_POST['apellidos'];
 @$dni=$_POST['dni'];
@@ -26,7 +28,7 @@ $existe="";
 
 	if(@$_POST['registro']){
 		
-	 if($check=="1"){
+	 if($check==="1"){
 		 if(empty($ciudad)||empty($direccion)||empty($dia)||empty($mes)||empty($anio)){
 			 $mensaje="<script> alert(' No puede dejar uno o mas estos campos vacios: ciudad, direccion y fecha de nacimiento ');</script>";
 		 }else{
@@ -47,7 +49,26 @@ $existe="";
 			
 		}
 		 
-	 }
+	 }else{
+         $ciudad="vacio";
+         $direccion="vacio";
+         $telefono="vacio";
+         $celular="vacio";
+         $nacimiento="1-1-1";
+			$existe=$trabajo->Exists_trabajador($dni,$email);
+		if(!$existe){
+	 		$foto="fotostrabaja/admin.jpg";
+	 		$bandeja=0;
+     		if($contra===$contra2){
+	   			$mensaje=$trabajo->set_trabajador($dni,$nombre,$apellidos,$email,$contra,$ciudad,$direccion,$telefono,$celular,$nacimiento,$foto,$bandeja);
+
+     		}else{
+        	$mensaje="<script> alert('Las contraseñas no coinciden');</script>";
+     		}
+		}else{
+			$mensaje="<script> alert(' DNI ó EMAIL ya existen ');</script>";
+		}
+     }
 		
 	
 		
